7d2108bed907226092145019344f7373df4e4fa2
debian <github.service@proslijedi.com>
2024-02-06 12:39:14 +0100

    add npm setup script

---
+++ scripts/setup_npm.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+rm -f .npmrc
+
+REPOSITORY_DOMAIN=sprovod
+REPOSITORY_DOMAIN_OWNER=336911096458
+REPOSITORY_REGION=eu-central-1
+REPOSITORY_NAMES=("sprovod-client" "sprovod-config")
+
+for REPOSITORY_NAME in "${REPOSITORY_NAMES[@]}"
+do
+    REPOSITORY_ENDPOINT=$(aws codeartifact get-repository-endpoint \
+        --domain $REPOSITORY_DOMAIN \
+        --repository $REPOSITORY_NAME \
+        --format npm \
+        --query repositoryEndpoint \
+        --output text)
+
+    echo "@$REPOSITORY_NAME:registry=$REPOSITORY_ENDPOINT" >> .npmrc
+    echo "${REPOSITORY_ENDPOINT#https:}:always-auth=true" >> .npmrc
+    echo "${REPOSITORY_ENDPOINT#https:}:_authToken=\${CODEARTIFACT_AUTH_TOKEN}" >> .npmrc
+    echo "" >> .npmrc
+done
+
+export CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token \
+    --domain $REPOSITORY_DOMAIN \
+    --domain-owner $REPOSITORY_DOMAIN_OWNER \
+    --region $REPOSITORY_REGION \
+    --query authorizationToken \
+    --output text)
+
+echo "$CODEARTIFACT_AUTH_TOKEN"
